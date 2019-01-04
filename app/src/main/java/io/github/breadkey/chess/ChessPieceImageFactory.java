53cf24d70df530eed922903d5a530415ed3a68bf
이영기 <t9rres@ajou.ac.kr>
2019-01-05 05:09:43 +0900

    체스말에따라 이미지 만들어주는 팩토리 추가

---
+++ app/src/main/java/io/github/breadkey/chess/ChessPieceImageFactory.java
@@ -0,0 +1,69 @@
+package io.github.breadkey.chess;
+
+import android.widget.Button;
+
+import io.github.breadkey.chess.model.ChessGame;
+import io.github.breadkey.chess.model.ChessPiece;
+
+public class ChessPieceImageFactory {
+    public static int createPieceImage(ChessPiece piece) {
+        ChessGame.Division division = piece.division;
+
+        switch (piece.type) {
+            case Pawn: {
+                if (division == ChessGame.Division.White) {
+                    return R.drawable.pawn_default_white;
+                }
+                else {
+                    return R.drawable.pawn_default_black;
+                }
+            }
+            case Rook: {
+                if (division == ChessGame.Division.White) {
+                    return R.drawable.rook_default_white;
+                }
+                else {
+                    return R.drawable.rook_default_black;
+                }
+            }
+
+            case Knight: {
+                if (division == ChessGame.Division.White) {
+                    return R.drawable.knight_default_white;
+                }
+                else {
+                    return R.drawable.knight_default_black;
+                }
+            }
+
+            case Bishop: {
+                if (division == ChessGame.Division.White) {
+                    return R.drawable.bishop_default_white;
+                }
+                else {
+                    return R.drawable.bishop_default_black;
+                }
+            }
+
+            case Queen: {
+                if (division == ChessGame.Division.White) {
+                    return R.drawable.queen_default_white;
+                }
+                else {
+                    return R.drawable.queen_default_black;
+                }
+            }
+
+            case King: {
+                if (division == ChessGame.Division.White) {
+                    return R.drawable.king_default_white;
+                }
+                else {
+                    return R.drawable.king_default_black;
+                }
+            }
+        }
+
+        return R.drawable.pawn_default_white;
+    }
+}
