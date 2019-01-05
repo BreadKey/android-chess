88e8f253253996326e3bf30ffe4480fff6f410af
이영기 <t9rres@ajou.ac.kr>
2019-01-05 23:43:06 +0900

    assertNull로 간결화

--- app/src/test/java/io/github/breadkey/chess/model/ChessGameTest.java
+++ app/src/test/java/io/github/breadkey/chess/model/ChessGameTest.java
@@ -27,7 +27,7 @@ public class ChessGameTest {
     public void move_a2PawnTo_a4() {
         chessGame.move('a', 2, 'a', 4);
         assertEquals(ChessPiece.Type.Pawn, chessGame.getPieceAt('a', 4).type);
-        assertEquals(null, chessGame.getPieceAt('a', 2));
+        assertNull(chessGame.getPieceAt('a', 2));
         assertEquals(ChessGame.Division.Black, chessGame.getCurrentTurn());
         printChessBoard();
     }
@@ -35,7 +35,7 @@ public class ChessGameTest {
     @Test
     public void move_a2PawnTo_a5WhichCanNotMove() {
         chessGame.move('a',2, 'a', 5);
-        assertEquals(null, chessGame.getPieceAt('a', 5));
+        assertNull(chessGame.getPieceAt('a', 5));
         assertEquals(ChessPiece.Type.Pawn, chessGame.getPieceAt('a', 2).type);
         assertEquals(ChessGame.Division.White, chessGame.getCurrentTurn());
     }
@@ -43,8 +43,8 @@ public class ChessGameTest {
     @Test
     public void move_a5To_a6WhichNullPiece() {
         chessGame.move('a', 5, 'a', 6);
-        assertEquals(null, chessGame.getPieceAt('a', 5));
-        assertEquals(null, chessGame.getPieceAt('a', 6));
+        assertNull(chessGame.getPieceAt('a', 5));
+        assertNull(chessGame.getPieceAt('a', 6));
         assertEquals(ChessGame.Division.White, chessGame.getCurrentTurn());
     }
 
