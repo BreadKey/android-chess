ddd23880afd85f6963e66d0267407ab3872194a8
이영기 <t9rres@ajou.ac.kr>
2019-01-09 13:12:48 +0900

    모든 체스말이 현재 위치를 가지고 있도록 변경

--- app/src/main/java/io/github/breadkey/chess/model/chess/chessPieces/King.java
+++ app/src/main/java/io/github/breadkey/chess/model/chess/chessPieces/King.java
@@ -4,27 +4,12 @@ import io.github.breadkey.chess.model.chess.ChessGame;
 import io.github.breadkey.chess.model.chess.ChessPiece;
 
 public class King extends ChessPiece {
-    private char file;
-    private  int rank;
     private boolean isChecked;
 
     public King(ChessGame.Division division) {
         super(division);
         type = Type.King;
         isChecked = false;
-    }
-
-    public void setCoordinate(char file, int rank) {
-        this.file = file;
-        this.rank = rank;
-    }
-
-    public char getFile() {
-        return file;
-    }
-
-    public int getRank() {
-        return rank;
     }
 
     public boolean isChecked() {
