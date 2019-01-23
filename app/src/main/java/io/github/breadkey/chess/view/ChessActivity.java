8fc70a8dd94124253b068b57b65363fa31d0ce1f
BreadKey <37973204+BreadKey@users.noreply.github.com>
2019-01-23 21:49:53 +0900

    기보보기 구현 (#27)

--- app/src/main/java/io/github/breadkey/chess/view/ChessActivity.java
+++ app/src/main/java/io/github/breadkey/chess/view/ChessActivity.java
@@ -2,30 +2,58 @@ package io.github.breadkey.chess.view;
 
 import android.content.Intent;
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.GridLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import java.util.HashMap;
 
 import io.github.breadkey.chess.R;
 import io.github.breadkey.chess.model.chess.ChessBoard;
 import io.github.breadkey.chess.model.chess.Coordinate;
+import io.github.breadkey.chess.model.chess.Move;
 import io.github.breadkey.chess.presenter.ChessPresenter;
 
 public class ChessActivity extends AppCompatActivity {
     ChessPresenter presenter;
     GridLayout chessSquareLayout;
     HashMap<Coordinate, SquareLayout> squareLayoutHashMap;
+    TableLayout moveTableLayout;
+    int moveCount;
+    HashMap<Integer, TableRow> moveRowHashMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        moveCount = 0;
+        moveRowHashMap = new HashMap<>();
         startActivity(new Intent(this, LoadingActivity.class));
         setContentView(R.layout.activity_chess);
         chessSquareLayout = findViewById(R.id.chess_square_layout);
         presenter = new ChessPresenter(this);
+
+        moveTableLayout = findViewById(R.id.move_table_layout);
+
+        Button showMoveButton = findViewById(R.id.show_move_button);
+        showMoveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DrawerLayout moveDrawerLayout = findViewById(R.id.move_drawer_layout);
+                if (!moveDrawerLayout.isDrawerOpen(Gravity.LEFT)) {
+                    moveDrawerLayout.openDrawer(Gravity.LEFT);
+                }
+                else {
+                    moveDrawerLayout.closeDrawer(Gravity.LEFT);
+                }
+            }
+        });
         createSquareButtons();
     }
 
@@ -57,6 +85,52 @@ public class ChessActivity extends AppCompatActivity {
         for (SquareLayout squareLayout: squareLayoutHashMap.values()) {
             squareLayout.getPieceButton().setBackgroundColor(Color.TRANSPARENT);
         }
+    }
+
+    public void addMoveRow(Move move) {
+        moveCount++;
+        if (isWhiteTurn()) {
+            TableRow moveRow = new TableRow(this);
+            int index = moveCount - (moveCount - 1) / 2;
+            TextView indexTextView = new TextView(this);
+            indexTextView.setText(String.valueOf(index) + ".");
+            indexTextView.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_END);
+            TextView whiteMoveTextView = createMoveTextView(move.toString());
+
+            moveRow.addView(indexTextView);
+            moveRow.addView(whiteMoveTextView);
+
+            moveTableLayout.addView(moveRow);
+            float dp = getResources().getDisplayMetrics().density;
+            int padding = (int) (2 * dp);
+            moveRow.setPadding(0, padding, 0, padding);
+
+            moveRowHashMap.put(index, moveRow);
+        }
+        else {
+            TableRow moveRow = moveRowHashMap.get(moveCount / 2);
+            TextView blackMoveTextView= createMoveTextView(move.toString());
+            moveRow.addView(blackMoveTextView);
+        }
+    }
+
+    private TextView createMoveTextView(String move) {
+        TextView moveTextView = new TextView(this);
+        moveTextView.setText(move);
+        moveTextView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+
+        return moveTextView;
+    }
+
+    private boolean isWhiteTurn() {
+        return moveCount % 2 == 1;
+    }
+
+    public void clearMoveTable() {
+        moveCount = 0;
+        TableRow columnSpacer = (TableRow) moveTableLayout.getChildAt(0);
+        moveTableLayout.removeAllViews();
+        moveTableLayout.addView(columnSpacer);
     }
 }
 
